993f3559c8a9fe62aa20a3e9fa422018b8adebb3
strblr <30187797+strblr@users.noreply.github.com>
2025-11-30 17:07:09 +0100

    refactor: improve layout style

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -23,65 +23,64 @@ export function Layout() {
   return (
     <>
       <Header onLayoutReset={handleLayoutReset} />
-      <ResizablePanelGroup
-        ref={mainLayoutRef}
-        className="gap-1"
-        direction="vertical"
-        autoSaveId="layout-main"
-      >
-        <ResizablePanel>
-          <ResizablePanelGroup
-            ref={topLayoutRef}
-            className="gap-1"
-            direction="horizontal"
-            autoSaveId="layout-top"
-          >
-            <ResizablePanel
-              order={1}
-              defaultSize={18}
-              minSize={18}
-              maxSize={60}
+      <Card className="overflow-hidden p-0">
+        <ResizablePanelGroup
+          ref={mainLayoutRef}
+          direction="vertical"
+          autoSaveId="layout-main"
+        >
+          <ResizablePanel>
+            <ResizablePanelGroup
+              ref={topLayoutRef}
+              direction="horizontal"
+              autoSaveId="layout-top"
             >
-              <Card className="h-full"></Card>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel order={2} className="min-w-xs">
-              <Card className="h-full"></Card>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel
-              order={3}
-              defaultSize={24}
-              minSize={18}
-              maxSize={60}
+              <ResizablePanel
+                order={1}
+                defaultSize={18}
+                minSize={18}
+                maxSize={60}
+              >
+                <div className="h-full"></div>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel order={2} className="min-w-xs">
+                <div className="h-full"></div>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel
+                order={3}
+                defaultSize={24}
+                minSize={18}
+                maxSize={60}
+              >
+                <div className="h-full"></div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel defaultSize={35} minSize={20} maxSize={70}>
+            <ResizablePanelGroup
+              ref={bottomLayoutRef}
+              direction="horizontal"
+              autoSaveId="layout-bottom"
             >
-              <Card className="h-full"></Card>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </ResizablePanel>
-        <ResizableHandle />
-        <ResizablePanel defaultSize={35} minSize={20} maxSize={70}>
-          <ResizablePanelGroup
-            ref={bottomLayoutRef}
-            className="gap-1"
-            direction="horizontal"
-            autoSaveId="layout-bottom"
-          >
-            <ResizablePanel
-              order={1}
-              defaultSize={18}
-              minSize={12}
-              maxSize={40}
-            >
-              <Card className="h-full"></Card>
-            </ResizablePanel>
-            <ResizableHandle />
-            <ResizablePanel order={2}>
-              <Card className="h-full"></Card>
-            </ResizablePanel>
-          </ResizablePanelGroup>
-        </ResizablePanel>
-      </ResizablePanelGroup>
+              <ResizablePanel
+                order={1}
+                defaultSize={18}
+                minSize={12}
+                maxSize={40}
+              >
+                <div className="h-full"></div>
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel order={2}>
+                <div className="h-full"></div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </Card>
     </>
   );
 }
